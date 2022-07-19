07c5e496aea3763ec3fecdc53da9ea8d6ba110f7
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-07-19 11:05:25 +0500

    created   binary   search   file

---
+++ binarySearch.js
@@ -0,0 +1,14 @@
+console.log('binary   search ');
+console.log("hello github ");
+let   list   =  [1,2,3,6,5,4]
+
+
+
+
+
+
+
+
+
+
+const  elt    =   document.getElementById('body').innerHTML= "binary"
