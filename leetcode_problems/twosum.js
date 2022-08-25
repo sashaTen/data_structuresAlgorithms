60ccbae0daaeafdc65d83f9e0a99c6710fe268ab
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-08-25 20:14:20 +0900

    just   logged  the   file

---
+++ leetcode_problems/twosum.js
@@ -0,0 +1,13 @@
+/*  
+function  accepts    target  and  the arr
+check  single  time
+function  that   is  called on  each  elt
+and returns  the   index   of    current and  the rest
+*/
+
+
+const   arr=     [2,7,11,15]
+const   targetOne   =     9
+const   targetTwo  =  13
+const   targetThree =   15+11
+console.log(targetThree);
