eb425d394a5724a77b6b7bde37aa62a6ae44b001
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-08-26 18:52:43 +0900

    solved  the   problems

--- leetcode_problems/check_duplicates.js
+++ leetcode_problems/check_duplicates.js
@@ -1,9 +1,36 @@
-const  checkDUPLICATS=()=>{
-    return    'hello  world  duplicats'
+
+
+
+
+const  checkDUPLICATS=(arr)=>{
+    
+    
+    function    isPresent(arr  ,   number){
+        for(let  i=0;  i < arr.length ;  i++){
+            if(arr[i]===number){
+                return   true
+            }
+        }      
+    }
+    
+
+    
+   let    newList   =   []
+
+    for(let   i  = 0 ;   i   <   arr.length ;  i++){
+          if(!isPresent(newList ,    arr[i])){
+
+              newList.push(arr[i])
+          }else   return   false
+
+    }
+    
+    return    true
 }
 
 
 console.log(
-    checkDUPLICATS()
+    checkDUPLICATS([1,2,6,8,100,1])
+    
 
 );
