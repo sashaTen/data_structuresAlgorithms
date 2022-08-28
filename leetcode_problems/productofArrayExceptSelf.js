d2bec4f59773b83b77e29309d0a11c6fb5dc36c3
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-08-28 18:52:41 +0900

    started    productofArrayExceptSelf.js

---
+++ leetcode_problems/productofArrayExceptSelf.js
@@ -0,0 +1,17 @@
+
+const  list    =  [1,2,3,4];
+const  productArrayExceptSelf=(arr)=>{
+let   list   =  [];
+
+}
+/*
+shift()
+and  the    sum  of   the  rest  of array 
+sum   of  elements  of   array 
+push it's  sum    in   the  element 
+
+
+*/
+
+
+console.log('238. Product of Array Except Self');
