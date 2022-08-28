67daeca5657cd5e837a5ce931615b2575b694f7d
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-08-28 19:34:09 +0900

    solved   product  of  array  except   itself

--- leetcode_problems/productofArrayExceptSelf.js
+++ leetcode_problems/productofArrayExceptSelf.js
@@ -2,16 +2,35 @@
 const  list    =  [1,2,3,4];
 const  productArrayExceptSelf=(arr)=>{
 let   list   =  [];
+for(let i  =0 ; i  <     arr.length ;  i++){
+    list.push(sumOfElts(arr)/arr[i])
 
 }
+return list
+
+}
+
+function    sumOfElts(arr){
+    let   sum    = 1;
+    for(let i  =0 ;    i   <     arr.length ;  i++){
+    sum*=arr[i]
+}
+
+return  sum 
+}
+
+
 /*
 shift()
 and  the    sum  of   the  rest  of array 
 sum   of  elements  of   array 
 push it's  sum    in   the  element 
-
+list
 
 */
 
+console.log(
+    productArrayExceptSelf(list)
 
-console.log('238. Product of Array Except Self');
+
+);
