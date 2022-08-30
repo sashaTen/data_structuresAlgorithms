04a1613f9b31eeb5666221a9b9f82d3d47045d19
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-08-30 19:05:52 +0900

    basic set   props

--- set.js
+++ set.js
@@ -1,1 +1,16 @@
-console.log('set   object   ');
+/*    
+very   basic    operations   are 
+add
+ delete 
+ has 
+ uniounSet
+*/
+
+
+
+let    setOne   =   new    Set([1,2,3,6,5])
+console.log(setOne);
+let   set2  =    new  Set()
+set2.add(111)
+set2.add(1)
+console.log(set2);
