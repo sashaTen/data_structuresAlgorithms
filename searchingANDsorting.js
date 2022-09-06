0dce133abdb1fa0fe3960672c31dd88ef6f149a7
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-09-06 21:04:54 +0900

    descending   order of  bubble  sort

--- searchingANDsorting.js
+++ searchingANDsorting.js
@@ -61,7 +61,7 @@ function    bubbleSort(arr){
 
         for(let   j  =0 ; j  <    arr.length-i-1 ;  j++)
 
-            if(arr[j]>arr[j+1]){
+            if(arr[j]<arr[j+1]){
                 let   temp  =   arr[j]
                 arr[j] =   arr[j+1]
                 arr[j+1] =   temp
