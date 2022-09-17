6d5ac390ed3adbd540c5a098f2e48d336f4f3767
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-09-17 19:37:27 +0900

    wrote  several   operations

--- doubleLINKEDlist.js
+++ doubleLINKEDlist.js
@@ -76,15 +76,102 @@ while(current.next){
 
 }
 
+function returnNODEbyINDEXfromEND(index){
+    if(index===0){
+        return  returnLASTnode()
+    }
+let  length  =   returnLENGTH()
+if(index >  length){
+    return  "too  big"
+}else if(index<0){
+return   "index  must  be  positive"
+}
+else   {
+let count   =  0;
+let  current =   returnLASTnode()
+while(current.prev){
+    count++
+    current =   current.prev
+    if (count   ===index){
+        return    current
+    }
 
-push(0)
+}
+}
+}
+
+
+function    insertAFTERindexFROMstart(index ,   value){
+    const   nodeBEFORE =   returnNODEbyINDEX(index)
+    const  node   =    new Node(value)
+    node.next    =   nodeBEFORE.next
+    node.prev =    nodeBEFORE
+    nodeBEFORE.next   =    node
+}
+
+
+function   insertAFTERindexFROMend(index ,    value){
+const  node   =   new  Node(value)
+const  nodeBYindex   =  returnNODEbyINDEXfromEND(index)
+node.next =   nodeBYindex.next
+node.prev  =  nodeBYindex
+nodeBYindex.next   =   node
+
+}
+function  deleteFROMstart(index){
+    const   nodeTOdelete  =    returnNODEbyINDEX(index)
+    const   nodeAFTER   =     nodeTOdelete.next
+    const   nodeBEFORE    =   nodeTOdelete.prev
+    nodeBEFORE.next   =   nodeTOdelete.next
+    nodeAFTER.prev  =   nodeBEFORE
+
+    
+}
+
+function   deleteFROMend(index){
+    const  nodeTOdelete =    returnNODEbyINDEXfromEND(index)
+    const   nodeAFTER   =     nodeTOdelete.next
+    const   nodeBEFORE    =   nodeTOdelete.prev
+    nodeBEFORE.next   =   nodeTOdelete.next
+    nodeAFTER.prev  =   nodeBEFORE
+
+}
+function   pringALLitems(){
+    let   arr =   []
+    let  current   =  head
+    while(current.next ){
+        arr.push(current)
+        current =  current.next
+    }
+    return   arr
+}
+function  printREVERSE(){
+    let  last   =   returnLASTnode()
+    let  arr= []
+    while(last.prev){
+        arr.push(last)
+        last  =   last.prev
+    }
+    return   arr
+}
+
+
+
 push(1)
-
 push(2)
 push(3)
 push(4)
-push(485)
-push(99996)
-returnLASTnode()
-console.log(returnNODEbyINDEX(8));
+push(5)
+
+push(1000)
+push(2000)
+push(3000)
+push(4000)
+push(5000)
+deleteFROMstart(3)
+deleteFROMend(2)
+console.log(printREVERSE());
+
+
+
 
