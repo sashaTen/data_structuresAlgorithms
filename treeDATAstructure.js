9370f79e48de7d3260fc5ec9eaac0b471a89f189
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-09-25 02:03:06 +0900

    search   functionality  was   implementted

--- treeDATAstructure.js
+++ treeDATAstructure.js
@@ -23,6 +23,11 @@ insert(value){
     }
 }
 
+
+returnROOT(){
+    return  this.root
+}
+
 insertNEWnode( currentNode, value){
 const    node   =    new  Node(value) 
 if(node.value >  currentNode.value){
@@ -43,6 +48,36 @@ else if(node.value<  currentNode.value){
 
 }
 
+
+searchNODEbyVALUE(value  ,    currentnode){
+if(value  ===   currentnode.value){
+    return     currentnode
+}
+else   if(value<     currentnode.value){
+     if(currentnode.left){
+    return   this.searchNODEbyVALUE(value   ,    currentnode.left)    
+    }
+    else   {
+
+        return    "node with  that  value does  not   exist"
+    }
+}
+else  if (value>    currentnode.value){
+    if(currentnode.right){
+        return    this.searchNODEbyVALUE(value  ,   currentnode.right)
+    }else   {
+    return    "node   with  that   value  does  not  exist "    
+    }
+    
+}
+
+
+
+
+
+}
+
+
 }
 
 
@@ -53,7 +88,7 @@ tree.insert(8)
 tree.insert(1198)
 tree.insert(2000)
 tree.insert(1540)
-console.log(tree);
+console.log(tree.searchNODEbyVALUE(20001 ,   tree.returnROOT() ));
 
 
 
